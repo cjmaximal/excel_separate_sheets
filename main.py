f25c17bc8fc2892b2677093c7df9c6b72ac2e6c0
Igor Ivanov <cjmaximal@gmail.com>
2022-05-24 15:38:01 +0300

    feat args: default out inplace

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import copy
 import os
 import openpyxl
@@ -14,7 +15,8 @@ def _get_files() -> list:
     return []
 
 
-def _save_sheet_as_new_workbook(workbook: openpyxl.Workbook, sheet_name: str, path: str):
+def _save_sheet_as_new_workbook(workbook: openpyxl.Workbook, workbook_name: str, sheet_name: str,
+                                is_separately: bool = False):
     new_workbook = copy.deepcopy(workbook)
     sheets = workbook.sheetnames
 
@@ -26,28 +28,37 @@ def _save_sheet_as_new_workbook(workbook: openpyxl.Workbook, sheet_name: str, pa
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
-    file_path = '{}/{}'.format(OUTPUT_PATH, path)
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
-
     today = date.today()
-    new_workbook.save('{}/{}_{}.xlsx'.format(file_path, sheet_name, today.strftime("%d_%m_%Y")))
+    file_path = '{}/{}'.format(OUTPUT_PATH, workbook_name)
+    if is_separately:
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        path_to_save = '{}/{}_{}.xlsx'.format(file_path, sheet_name, today.strftime("%d_%m_%Y"))
+    else:
+        path_to_save = '{}_{}_{}.xlsx'.format(file_path, sheet_name, today.strftime("%d_%m_%Y"))
+    new_workbook.save(path_to_save)
 
 
-def _separate_file(file: str):
+def _separate_file(file: str, is_separately: bool = False):
     file_path = '{}/{}'.format(SOURCE_PATH, file)
     workbook = openpyxl.load_workbook(file_path)
     sheets = workbook.sheetnames
 
-    file_path = file.split('.')[0]
+    workbook_name = file.split('.')[0]
     for sheet in sheets:
-        _save_sheet_as_new_workbook(workbook, sheet, file_path)
+        _save_sheet_as_new_workbook(workbook, workbook_name, sheet, is_separately)
 
 
 def run():
+    parser = argparse.ArgumentParser(description='Separate workbook sheets as self workbooks.')
+    parser.add_argument('--by-folders', dest='is_separately', action='store_const',
+                        const=True, default=False,
+                        help='Out workbooks separately by folders (default: place all results to out directory)')
+    args = parser.parse_args()
+
     source_files = _get_files()
     for file in source_files:
-        _separate_file(file)
+        _separate_file(file, args.is_separately)
 
 
 # Press the green button in the gutter to run the script.
